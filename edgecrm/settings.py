f87ab3a9823bad40334774d84f4ab871ce417403
Soham Navadkar <navadkarsoham191@gmail.com>
2024-02-25 20:59:44 +0530

    deploy2

--- edgecrm/settings.py
+++ edgecrm/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- edgecrm/wsgi.py
+++ edgecrm/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.middleware import WhiteNoiseMiddleware
+
+application = get_wsgi_application()
+application = WhiteNoiseMiddleware(application)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edgecrm.settings')
 
