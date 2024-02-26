62ebf375cd13be72fa216e5cb590a0e2e91bb249
Soham Navadkar <navadkarsoham191@gmail.com>
2024-02-27 00:01:29 +0530

    z

--- edgecrm/wsgi.py
+++ edgecrm/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.middleware import WhiteNoiseMiddleware
 
 application = get_wsgi_application()
-application = WhiteNoiseMiddleware(application)
+application = WhiteNoiseMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edgecrm.settings')
 
