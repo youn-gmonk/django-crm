18b170d422fddcc4ed95db0dbe9ac32a203f451a
Soham Navadkar <navadkarsoham191@gmail.com>
2024-02-27 22:48:43 +0530

    a

--- edgecrm/settings.py
+++ edgecrm/settings.py
@@ -164,9 +164,9 @@ if not DEBUG:
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 465
-    EMAIL_USE_TLS = False
-    EMAIL_USE_SSL = True
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
 
     EMAIL_HOST_USER = 'navadkarsoham191@gmail.com'
     EMAIL_HOST_PASSWORD = 'fidi pkxi jllv uhdy'
