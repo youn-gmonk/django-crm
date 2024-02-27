96ac76b9a2abe6ef446305335247a7bc8e268815
Soham Navadkar <navadkarsoham191@gmail.com>
2024-02-27 11:43:35 +0530

    z

--- edgecrm/settings.py
+++ edgecrm/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
