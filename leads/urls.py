148c9f9535bd1bf89c8172c0aac01d16884e10f2
Soham Navadkar <navadkarsoham191@gmail.com>
2024-02-27 20:38:05 +0530

    a

--- leads/urls.py
+++ leads/urls.py
@@ -9,7 +9,7 @@ from .views import (
 app_name = "leads"
 
 urlpatterns = [
-    path('', LeadListView.as_view() , name='lead-list'),
+    path('/', LeadListView.as_view() , name='lead-list'),
     path('<int:pk>/', LeadDetailView.as_view() , name='lead-detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view() , name='lead-update'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
